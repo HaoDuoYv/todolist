9cb5dfd7c5c455c3fe1f154ee44d56f687bfb6b0
HaoDuoYv <3087279776@qq.com>
2025-12-20 00:04:08 +0800

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+
+
+
+
+const routes = [
+
+  {
+    path: '/',
+    component: () => import('../App.vue'),
+    meta: {
+      title: '´ı°ìÊÂÏî'
+    }
+
+  }
+]
+export default router
