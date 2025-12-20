64aa31c237d9382a0157884a644a8058a2873e78
HaoDuoYv <3087279776@qq.com>
2025-12-20 14:52:03 +0800

    icon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.eokt1qk9jjo"
+    "revision": "0.so9go7vh668"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
